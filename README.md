3a44ba0f8fe6bb813e8a52f687f3160d328a15dd
Sh1nePr0 <fedoreev77@mail.ru>
2024-07-02 21:38:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+This is my new RPG game about the knight Roland, who was the owner of the great Durandal
---
+++ Source/TheGreatDurandal/TheGreatDurandalGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheGreatDurandalGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATheGreatDurandalGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATheGreatDurandalGameMode();
+};
+
+
+
