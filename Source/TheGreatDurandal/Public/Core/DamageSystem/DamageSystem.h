b06b8848be971bd99e6753821ce82e45ea873709
Sh1nePr0 <fedoreev77@mail.ru>
2024-08-09 01:32:13 +0300

    Create new folders and moved bp classes there, create classes for damage and health system

---
+++ Source/TheGreatDurandal/Public/Core/DamageSystem/DamageSystem.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "DamageSystem.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class THEGREATDURANDAL_API UDamageSystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+};
